0e53095a6c0668635f6e965a54eb242c346ec800
ZOmohundro <zaneomohundro@gmail.com>
2024-04-14 14:01:46 -0400

    add ability to delete

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -2,7 +2,14 @@ function ProjectList() {
   const projects = [];
 
   const addProject = (project) => projects.push(project);
-  const deleteProject = () => {};
+
+  const deleteProject = (projectId) => {
+    console.log("projectId: ", projectId);
+    const result = projects.findIndex((obj) => obj["id"] === projectId);
+    console.log(result);
+    projects.splice(result, 1);
+  };
+
   const getProjects = () => projects;
 
   return { addProject, deleteProject, getProjects };
