df6e6cbe15b1dabc769b458342cabb438942113c
ZOmohundro <zaneomohundro@gmail.com>
2024-04-16 20:31:14 -0400

    rough in edit task on click

--- src/View.js
+++ src/View.js
@@ -31,12 +31,15 @@ function View() {
     const newUl = document.createElement("ul");
     project.tasks.forEach((task) => {
       const newLi = document.createElement("li");
+      newLi.id = task.id;
       newLi.innerText = `${task.title} - ${task.description}`;
+      newLi.addEventListener("click", () => editTask(task));
 
       newUl.appendChild(newLi);
       mainContentDiv.appendChild(newUl);
     });
 
+    // Add new task functionality
     const newTaskLi = document.createElement("li");
     const newTaskBtn = document.createElement("button");
     newTaskBtn.innerText = "Add New Task";
@@ -44,7 +47,50 @@ function View() {
     newTaskLi.appendChild(newTaskBtn);
     newUl.appendChild(newTaskLi);
   };
-  return { updateProjectView, updateTaskView };
+
+  const editTask = (task) => {
+    //replace existing DOM element with full details
+    const taskLi = document.getElementById(task.id);
+    console.log("edit task clicked", task);
+
+    // container div
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("task-details");
+
+    // Title input field
+    const titleInput = document.createElement("input");
+    titleInput.value = task.title;
+    taskDiv.appendChild(titleInput);
+
+    // Description input field
+    const descriptionInput = document.createElement("textarea");
+    descriptionInput.value = task.description || ""; // If description is not provided, initialize with empty string
+    descriptionInput.placeholder = "Description";
+    taskDiv.appendChild(descriptionInput);
+
+    // Due date input field
+    const dueDateInput = document.createElement("input");
+    dueDateInput.type = "date";
+    dueDateInput.value = task.dueDate || ""; // If due date is not provided, initialize with empty string
+    dueDateInput.placeholder = "Due Date";
+    taskDiv.appendChild(dueDateInput);
+
+    // Priority input field
+    const priorityInput = document.createElement("select");
+    const priorityOptions = ["Low", "Medium", "High"];
+    priorityOptions.forEach((option) => {
+      const priorityOption = document.createElement("option");
+      priorityOption.text = option;
+      priorityOption.value = option.toLowerCase();
+      priorityInput.appendChild(priorityOption);
+    });
+    priorityInput.value = task.priority || ""; // If priority is not provided, initialize with empty string
+    taskDiv.appendChild(priorityInput);
+
+    taskLi.parentNode.replaceChild(taskDiv, taskLi);
+  };
+
+  return { updateProjectView, updateTaskView, editTask };
 }
 
 export default View;
